b4f7b8d6f0e4a3d8b4725480b9d2b549049220d4
imran <zunaeede@gmail.com>
2023-08-29 10:22:08 +0600

    msin -object practice - constructor function

---
+++ object.php
@@ -0,0 +1,16 @@
+<?php
+ 
+class employee{
+   public $name;
+   public $salary;
+
+   function __construct()
+   {
+    echo "this is test constructor test function"."<br>";
+   }
+}
+$imran = new employee();
+$dip = new employee();
+$akash = new employee();
+
+?>
